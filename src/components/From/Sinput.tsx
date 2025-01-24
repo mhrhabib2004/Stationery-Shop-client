40afaa2175164a57e73ef27396e3a151a314770b
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-25 05:56:47 +0600

    up:input

--- src/components/From/Sinput.tsx
+++ src/components/From/Sinput.tsx
@@ -1,20 +1,28 @@
-import { Controller } from "react-hook-form";
-import { Form } from "react-router-dom";
+// import { Controller } from "react-hook-form";
+// import { Form } from "react-router-dom";
 
-type TInputProps = {
-    type: string;
-    name: string;
-    label?: string;
-  };
+// type TInputProps = {
+//     type: string;
+//     name: string;
+//     label?: string;
+//   };
   
-export default function Sinput({ type, name, label }: TInputProps) {
-  return (
-    <div >
+// export default function Sinput({ type, name, label }: TInputProps) {
+//   return (
+//     <div >
      
-      <Controller
-        name={name}
-        render={({ field }) => <Form.Item label={label}><Input {...field} type={type} id={name} /></Form.Item> }
-      />
-    </div>
+//       {/* <Controller
+//         name={name}
+        
+//         // render={({ field }) => <Form.Item label={label}><Input {...field} type={type} id={name} /></Form.Item> }
+//       /> */}
+//     </div>
+//   )
+// }
+
+
+export default function Sinput() {
+  return (
+    <div>Sinput</div>
   )
 }
