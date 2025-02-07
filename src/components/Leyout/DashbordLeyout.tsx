d18970e41c71869ccf86b2037da56a07b84134b3
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-02-07 22:56:48 +0600

    up:dasbordleyout

--- src/components/Leyout/DashbordLeyout.tsx
+++ src/components/Leyout/DashbordLeyout.tsx
@@ -1,7 +1,11 @@
+import {  useAppSelector } from "../../redux/hooks";
 import Admindasbord from "../Dashbord/AdminDasbord";
 
 
 export default function DashbordLeyout() {
+  // const dispatch = useAppDispatch()
+  const user = useAppSelector((state) => state.auth.user);
+  console.log(user)
   return (
     <div>
 <Admindasbord />
