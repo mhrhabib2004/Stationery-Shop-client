dd1e0614b8598934ab70f38416fc642cd0a994f0
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-02-07 23:50:34 +0600

    up:dashbordleyout

--- src/components/Leyout/DashbordLeyout.tsx
+++ src/components/Leyout/DashbordLeyout.tsx
@@ -1,14 +1,22 @@
-import {  useAppSelector } from "../../redux/hooks";
+import { useAppSelector } from "../../redux/hooks";
 import Admindasbord from "../Dashbord/AdminDasbord";
+import UserDasbord from "../Dashbord/UserDasbord";
 
+// Define the type for the user object (adjust according to your Redux state structure)
+interface User {
+  role: 'user' | 'admin';
+}
 
-export default function DashbordLeyout() {
-  // const dispatch = useAppDispatch()
+export default function DashbordLayout() {
+  // Use the `useAppSelector` hook to get the user from the Redux store
   const user = useAppSelector((state) => state.auth.user);
-  console.log(user)
+
+  console.log(user?.role);
+
   return (
     <div>
-<Admindasbord />
+      {user?.role === 'user' && <UserDasbord />}
+      {user?.role === 'admin' && <Admindasbord />}
     </div>
-  )
+  );
 }
