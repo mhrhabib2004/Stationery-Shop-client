45dfa26b5190e4cb01abf4e624f7f7bf668293ae
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-02-20 00:46:49 +0600

    up:userdasbord

--- src/components/Dashbord/userDashbord/UserDasbord.tsx
+++ src/components/Dashbord/userDashbord/UserDasbord.tsx
@@ -20,7 +20,7 @@ export default function UserDasbord() {
       <div className="col-span-1 bg-white">
         <div className="p-2 h-full w-full flex flex-col bg-white dark:bg-gray-900 border-r border-r-gray-200">
           {/* Logo */}
-          <a href="#">
+          <a href="/">
             <div className="flex text-md md:text-2xl flex-col md:flex-row justify-center lg:justify-start font-bold items-center gap-1 py-2 px-0 md:px-2 lg:px-4 cursor-pointer">
               <span>Stationery</span>
               <span>Shop</span>
@@ -39,7 +39,7 @@ export default function UserDasbord() {
               </div>
               <a
                 className="flex flex-row items-center justify-center lg:justify-start rounded-md h-12 focus:outline-none pr-3.5 lg:pr-6 font-semibold text-gray-500 hover:text-primary-400 cursor-pointer"
-                href="/dashboard/orders" // ঠিক করতে হবে
+                href="/dashboard/view-orders" 
               >
                 <span className="inline-flex justify-center items-center ml-3.5">
                   <RiListView size={20} />
